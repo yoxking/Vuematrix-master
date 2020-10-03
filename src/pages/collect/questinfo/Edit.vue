4282762201f5bd6ba88cc970fdf375c7f13185a0
yoxking <yoxking@hotmail.com>
2020-10-03 08:46:37 +0800

    updated 20201003

--- src/pages/collect/questinfo/Edit.vue
+++ src/pages/collect/questinfo/Edit.vue
@@ -1,122 +1,148 @@
 <template>
   <div class="wrapbox">
-    <br />
     <a-form-model ref="ruleForm"
                   :model="form"
                   :rules="rules"
                   :label-col="labelCol"
                   :wrapper-col="wrapperCol">
-      <a-row>
-        <a-col :span="spanCol">
-          <a-form-model-item label="编号"
-                             prop="questNo"
-                             ref="questNo">
-            <a-input v-model="form.questNo"
-                     readOnly />
-          </a-form-model-item>
-        </a-col>
-        <a-col :span="spanCol">
-          <a-form-model-item label="类别"
-                             prop="questType"
-                             ref="questType">
-            <a-radio-group v-model="form.questType">
-              <a-radio value="1">填空题</a-radio>
-              <a-radio value="2">单选题</a-radio>
-              <a-radio value="3">多选题</a-radio>
-            </a-radio-group>
-          </a-form-model-item>
-        </a-col>
-      </a-row>
-      <a-row>
-        <a-col :span="24">
-          <a-form-model-item label="题目"
-                       prop="questTitle"
-                       ref="questTitle"
-                       :labelCol="{span: 3}"
-                       :wrapperCol="{span: 20}">
-            <a-input v-model="form.questTitle" />
-          </a-form-model-item>
-        </a-col>
-      </a-row>
-      <a-row>
-        <a-col :span="spanCol">
-          <a-form-model-item label="类型"
-                             prop="classNo"
-                             ref="classNo">
-            <a-tree-select v-model="form.classNo"
-                           :multiple="false"
-                           :allow-clear="false"
-                           :show-search="false"
-                           :tree-data="treeData"
-                           placeholder="请选择类型" />
-          </a-form-model-item>
-        </a-col>
-        <a-col :span="spanCol">
-          <a-form-model-item label="是否必填"
-                             prop="questMust"
-                             ref="questMust">
-            <a-radio-group v-model="form.questMust">
-              <a-radio value="0">非必填</a-radio>
-              <a-radio value="1">必填</a-radio>
-            </a-radio-group>
-          </a-form-model-item>
-        </a-col>
-      </a-row>
-      <a-row>
-        <a-col :span="24">
-          <a-form-model-item label="描述"
-                       prop="questDesc"
-                       ref="questDesc"
-                       :labelCol="{span: 3}"
-                       :wrapperCol="{span: 20}">
-            <a-textarea v-model="form.questDesc"
-                        placeholder="简介"
-                        :autoSize="{ minRows: 3, maxRows: 5 }" />
-          </a-form-model-item>
-        </a-col>
-      </a-row>
-      <a-row>
-        <a-col :span="spanCol">
-          <a-form-model-item label="总得分"
-                             prop="questScore"
-                             ref="questScore">
-            <a-input-number v-model="form.questScore" />
-          </a-form-model-item>
-        </a-col>
-        <a-col :span="spanCol">
-          <a-form-model-item label="排序"
-                             prop="orderNo"
-                             ref="orderNo">
-            <a-input-number v-model="form.orderNo" />
-          </a-form-model-item>
-        </a-col>
-      </a-row>
-      <div v-if="form.questType!=='1'">
-        <a-row v-for="(item,index) in form.options"
-               :key="item.optNo">
-          <a-col :span="spanCol">
-            <a-form-model-item :label="'选项'+(index+1)">
-              <a-input v-model="item.optTitle" />
-            </a-form-model-item>
-          </a-col>
-          <a-col :span="spanCol">
-            <a-form-model-item label="得分">
-              <a-input-number v-model="item.optScore"
-                              style="margin-right:10px;" />
-              <a-button type="primary"
-                        shape="circle"
-                        size="small"
-                        icon="plus"
-                        @click="onAddOpt(index)" />
-              <a-button type="danger"
-                        shape="circle"
-                        size="small"
-                        icon="minus"
-                        @click="onDelOpt(index)" />
-            </a-form-model-item>
-          </a-col>
-        </a-row>
-      </div>
+      <a-tabs default-active-key="1">
+        <a-tab-pane key="1"
+                    tab="基本信息">
+          <a-row>
+            <a-col :span="spanCol">
+              <a-form-model-item label="编号"
+                                 prop="questNo"
+                                 ref="questNo">
+                <a-input v-model="form.questNo"
+                         readOnly />
+              </a-form-model-item>
+            </a-col>
+            <a-col :span="spanCol">
+              &nbsp;
+            </a-col>
+          </a-row>
+          <a-row>
+            <a-col :span="spanCol">
+              <a-form-model-item label="类型"
+                                 prop="classNo"
+                                 ref="classNo">
+                <a-tree-select v-model="form.classNo"
+                               :multiple="false"
+                               :allow-clear="false"
+                               :show-search="false"
+                               :tree-data="treeData"
+                               placeholder="请选择类型" />
+              </a-form-model-item>
+            </a-col>
+            <a-col :span="spanCol">
+              <a-form-model-item label="排序"
+                                 prop="orderNo"
+                                 ref="orderNo">
+                <a-input-number v-model="form.orderNo" />
+              </a-form-model-item>
+            </a-col>
+          </a-row>
+          <a-row>
+            <a-col :span="spanCol">
+              <a-form-model-item label="类别"
+                                 prop="questType"
+                                 ref="questType">
+                <a-radio-group v-model="form.questType">
+                  <a-radio value="1">填空题</a-radio>
+                  <a-radio value="2">单选题</a-radio>
+                  <a-radio value="3">多选题</a-radio>
+                </a-radio-group>
+              </a-form-model-item>
+            </a-col>
+            <a-col :span="spanCol">
+              &nbsp;
+            </a-col>
+          </a-row>
+          <a-row>
+            <a-col :span="spanCol">
+              <a-form-model-item label="是否必填"
+                                 prop="questMust"
+                                 ref="questMust">
+                <a-radio-group v-model="form.questMust">
+                  <a-radio value="0">非必填</a-radio>
+                  <a-radio value="1">必填</a-radio>
+                </a-radio-group>
+              </a-form-model-item>
+            </a-col>
+            <a-col :span="spanCol">
+              <a-form-model-item label="总得分"
+                                 prop="questScore"
+                                 ref="questScore">
+                <a-input-number v-model="form.questScore" />
+              </a-form-model-item>
+            </a-col>
+          </a-row>
+          <a-row>
+            <a-col :span="24">
+              <a-form-item label="备注"
+                           :labelCol="{span: 3}"
+                           :wrapperCol="{span: 20}">
+                <a-textarea v-model="form.comments"
+                            placeholder="备注信息"
+                            :autoSize="{ minRows: 3, maxRows: 5 }" />
+              </a-form-item>
+            </a-col>
+          </a-row>
+        </a-tab-pane>
+        <a-tab-pane key="2"
+                    tab="配置信息">
+          <a-row>
+            <a-col :span="24">
+              <a-form-model-item label="题目"
+                                 prop="questTitle"
+                                 ref="questTitle"
+                                 :labelCol="{span: 3}"
+                                 :wrapperCol="{span: 20}">
+                <a-input v-model="form.questTitle" />
+              </a-form-model-item>
+            </a-col>
+          </a-row>
+          <a-row>
+            <a-col :span="24">
+              <a-form-model-item label="描述"
+                                 prop="questDesc"
+                                 ref="questDesc"
+                                 :labelCol="{span: 3}"
+                                 :wrapperCol="{span: 20}">
+                <a-textarea v-model="form.questDesc"
+                            placeholder="简介"
+                            :autoSize="{ minRows: 3, maxRows: 5 }" />
+              </a-form-model-item>
+            </a-col>
+          </a-row>
+          <div v-if="form.questType!=='1'">
+            <a-row v-for="(item,index) in form.options"
+                   :key="item.optNo">
+              <a-col :span="spanCol">
+                <a-form-model-item :label="'选项'+(index+1)">
+                  <a-input v-model="item.optTitle" />
+                </a-form-model-item>
+              </a-col>
+              <a-col :span="spanCol">
+                <a-form-model-item label="得分">
+                  <a-input-number v-model="item.optScore"
+                                  style="margin-right:10px;" />
+                  <a-button type="primary"
+                            shape="circle"
+                            size="small"
+                            icon="plus"
+                            @click="onAddOpt(index)" />
+                  <a-button type="danger"
+                            shape="circle"
+                            size="small"
+                            icon="minus"
+                            @click="onDelOpt(index)" />
+                </a-form-model-item>
+              </a-col>
+            </a-row>
+          </div>
+        </a-tab-pane>
+      </a-tabs>
     </a-form-model>
     <a-divider />
     <div class="btnbox">
